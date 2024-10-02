2ce48919741452188d7ec2384f97c27b1bd47d15
kareemelewa2002 <elewakareem2002@gmail.com>
2024-10-02 10:02:04 +0300

    Added front-end for Tourism Governor with museum form and list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import MuseumForm from './components/MuseumForm';
 import MuseumList from './components/MuseumList';
+import MyActivities from './components/MyActivities';
+import './index.css';
 
 function App() {
     const [museums, setMuseums] = useState([]);
@@ -11,10 +13,20 @@ function App() {
     };
 
     return (
-        <div>
-            <h1>Tourism Governor</h1>
-            <MuseumForm onSubmit={handleAddMuseum} />
-            <MuseumList museums={museums} />
+        <div className="App">
+            <header>
+                <h1>Tourism Governor Dashboard</h1>
+            </header>
+            <main>
+                {/* Museum management */}
+                <h2>Add and Manage Museums</h2>
+                <MuseumForm onSubmit={handleAddMuseum} />
+                <MuseumList museums={museums} />
+
+                {/* Created Activities section */}
+                <h2>My Created Activities</h2>
+                <MyActivities />
+            </main>
         </div>
     );
 }
