ac37baafd404cd5761ebd48838769fb60c475c75
Oliverashraf <oliverashraf4@gmail.com>
2024-10-04 21:59:16 +0300

    governor update

--- Backend/controllers/GovernorController.js
+++ Backend/controllers/GovernorController.js
@@ -38,20 +38,6 @@ const getAllGovernors = async (req, res) => {
 
 
 
-const museumSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }, 
-    description: { type: String, required: true },
-    pictures: [{ type: String }], 
-    location: { type: String, required: true },
-    openingHours: { type: String, required: true },
-    ticketPrices: {
-        native: { type: Number, required: true },
-        foreigner: { type: Number, required: true },
-        student: { type: Number, required: true }
-    }
-});
-
-const Museum = mongoose.model('Museum', museumSchema);
 
 
 const createMuseum = async (req, res) => {
