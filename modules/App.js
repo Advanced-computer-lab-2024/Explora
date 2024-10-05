9cb23387795896adba19cf92e185d65192d54486
youssefe1haddad <yoyo58296@gmail.com>
2024-10-05 17:15:43 +0300

    Update App.js

--- modules/App.js
+++ modules/App.js
@@ -1,16 +1,31 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import TouristProfile from '/Users/youssefelhaddad/Explora/my-app/src/TouristProfile'; // Adjust path if necessary
-import SearchPage from './TouristSearch';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TouristProfile from './TouristProfile'; // Adjust path if necessary
+import SearchPageHeader from './SearchPageHeader';
+import SiteSearchPage from './SiteSearchPage'; // Your search components
+import ActivitySearchPage from './ActivitySearchPage';
+import ItinerarySearchPage from './ItinerarySearchPage';
+import UpcomingActivities from './UpcomingActivities';
+import UpcomingItineraries from './UpcomingItineraries';
+
 
 function App() {
   return (
-      <div className="App">
-          <h1>Tourist Profile Page</h1>
-          <TouristProfile />
-          <SearchPage />
-      </div>
+    <Router>
+      <div>
+       <Routes>
+        <Route path="/" element={<TouristProfile/>} /> 
+        <Route path="/SearchPageHeader" element={<SearchPageHeader/>} />
+        <Route path="/SiteSearchPage" element={<SiteSearchPage/>} />
+        <Route path="/ActivitySearchPage" element={<ActivitySearchPage/>} />
+        <Route path="/ItinerarySearchPage" element={<ItinerarySearchPage/>} />
+        <Route path="/UpcomingActivities" element={<UpcomingActivities/>} />
+        <Route path="/UpcomingItineraries" element={<UpcomingItineraries/>} />
+       </Routes>
+     </div>
+    </Router>
   );
 }
 
