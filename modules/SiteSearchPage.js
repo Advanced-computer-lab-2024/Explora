ee9772e8dc79747988c2bc1a2cd680634e37d11c
youssefe1haddad <yoyo58296@gmail.com>
2024-10-05 17:24:15 +0300

    Create SiteSearchPage.js

---
+++ modules/SiteSearchPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const SiteSearchPage = () => {
+  return (
+    <div>
+      <h2>Site Search Page</h2>
+      <p>Here you can search for sites.</p>
+    </div>
+  );
+};
+
+export default SiteSearchPage;
