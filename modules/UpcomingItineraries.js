1f866cbe18e739d1c319871daa06093d3fe1dd96
youssefe1haddad <yoyo58296@gmail.com>
2024-10-05 17:27:00 +0300

    Create UpcomingItineraries.js

---
+++ modules/UpcomingItineraries.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const UpcomingItineraries = () => {
+  return (
+    <div>
+      <h1>Here Are Our Upcoming Itineraries!</h1>
+      
+    </div>
+  );
+};
+
+export default UpcomingItineraries;
