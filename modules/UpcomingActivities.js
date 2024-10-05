b1e245c4434863b7110636a47cfb85a4f08256f6
youssefe1haddad <yoyo58296@gmail.com>
2024-10-05 17:25:45 +0300

    Create UpcomingActivities.js

---
+++ modules/UpcomingActivities.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const UpcomingActivities = () => {
+  return (
+    <div>
+      <h1>Here Are Our Upcoming Activities!</h1>
+      
+    </div>
+  );
+};
+
+export default UpcomingActivities;
