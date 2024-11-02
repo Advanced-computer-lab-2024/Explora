16bf4062cabf677a907c9a89c1464dd33498bf62
youssefe1haddad <yoyo58296@gmail.com>
2024-11-02 16:43:01 +0200

    Update UpcomingItineraries.js
    Same as edits applied to UpcomingActivities.jsx

--- modules/UpcomingItineraries.js
+++ modules/UpcomingItineraries.js
@@ -1,27 +1,51 @@
 import React from 'react';
 
-
+import { useState, useEffect } from 'react';
 
 const UpcomingItineraries = () => {
+
+  const [itins,setItins]=useState([])
+
+  useEffect(() => {
+  fetch('http://localhost:4000/api/tour_guide_itinerary/upcoming').then(response => response.json()).then(data => {  
+   
+  })
+  
+  },[])
+
   return (
     <div>
-      <h1>Here Are Our Upcoming Itineraries!</h1>
-      <p1>
+   {  itins.map((place) => ( 
+      <div key={place._id} className="place">
+        <h3>{place.name}</h3>
+        <p>date: {place.date}</p>
+        <p>{place.price}$</p>
+        <p>rating: {place.rating}/10</p>
 
-       Tour (available in 3 languages) to Egyptian Museum ON THE 10TH OF OCTOBER <br />
-
-       Tour (available in 3 languages) to Pyramids of Giza ON NOVEMBER THE 1ST <br />
-
-       Tour (only available in  Arabic) to Art Museum ON THE 20TH OF SEPTEMBER <br />
-
-       Tour (only available in Arabic) to Cairo Tower ON THE 5TH OF DECEMBER <br />
-
-       Tour (available in two languages) to Luxor Temple ON THE 25TH OF OCTOBER 
-
-       </p1>
       
+      </div>
+     ))}
+     <div>
+       {/* Link button your bookings */}
+       <Link to="/UpcomingBookings">
+          <button style={buttonStyle}>View Your Bookings</button>
+        </Link>
+     </div>
+
     </div>
   );
 };
 
+const buttonStyle = {
+  margin: '0 10px',
+  padding: '10px 20px',
+  fontSize: '16px',
+  cursor: 'pointer',
+  borderRadius: '5px',
+  border: '1px solid #ccc',
+};
+
 export default UpcomingItineraries;
+
+
+
