3091830b146548c7badec1053c89134323471c88
youssefe1haddad <yoyo58296@gmail.com>
2024-11-09 21:27:57 +0200

    Update UpcomingItineraries.js

--- modules/UpcomingItineraries.js
+++ modules/UpcomingItineraries.js
@@ -1,48 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const UpcomingItineraries = () => {
-
-  const [itins,setItins]=useState([])
+  const [itins, setItins] = useState([]);
 
   useEffect(() => {
-  fetch('http://localhost:4000/api/tour_guide_itinerary/upcoming').then(response => response.json()).then(data => {  
-   
-  })
-  
-  },[])
+    fetch('http://localhost:4000/api/tour_guide_itinerary/upcoming')
+      .then(response => response.json())
+      .then(data => {
+        data = data.map((itin) => {
+          // Check if 'date' exists before trying to split it
+          const formattedDate = itin.date ? itin.date.split('T')[0] : 'No Date Available';
+          return { ...itin, date: formattedDate };
+        });
+        console.log(data);
+        setItins(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <div>
-   {  itins.map((place) => ( 
-      <div key={place._id} className="place">
-        <h3>{place.name}</h3>
-        <p>date: {place.date}</p>
-        <p>{place.price}$</p>
-        <p>rating: {place.rating}/10</p>
-
-      
-      </div>
-     ))}
-     <div>
-       {/* Link button your bookings */}
-       <Link to="/UpcomingBookings">
-          <button style={buttonStyle}>View Your Bookings</button>
-        </Link>
-     </div>
-
+      {itins.map((itin) => (
+        <div key={itin._id} className="itin">
+          <h3>{itin.name}</h3>
+          <p>date: {itin.date}</p>
+          <p>{itin.price}$</p>
+          <p>rating: {itin.rating}/10</p>
+        </div>
+      ))}
+      <Link to="/EventActivityItineraryBooking">
+        <button style={{ padding: '10px', margin: '10px', fontSize: '16px' }}>Book a ticket to an itinerary</button>
+      </Link>
     </div>
   );
-};
-
-const buttonStyle = {
-  margin: '0 10px',
-  padding: '10px 20px',
-  fontSize: '16px',
-  cursor: 'pointer',
-  borderRadius: '5px',
-  border: '1px solid #ccc',
 };
 
 export default UpcomingItineraries;
