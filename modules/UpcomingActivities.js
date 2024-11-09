4113130a6bfa10ec7028bbcffcbffe8197bd30ec
youssefe1haddad <yoyo58296@gmail.com>
2024-11-09 21:27:29 +0200

    Update UpcomingActivities.js

--- modules/UpcomingActivities.js
+++ modules/UpcomingActivities.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import { useState, useEffect } from 'react';
 
 const UpcomingActivities = () => {
@@ -16,36 +16,20 @@ const UpcomingActivities = () => {
   },[])
 
   return (
-  <div>
+    <div>
    {  places.map((place) => ( 
       <div key={place._id} className="place">
         <h3>{place.name}</h3>
         <p>date: {place.date}</p>
         <p>{place.price}$</p>
         <p>rating: {place.rating}/10</p>
-
-       
       </div>
      ))}
-     <div>
-       {/* Link button your bookings */}
-       <Link to="/UpcomingBookings">
-          <button style={buttonStyle}>View Your Bookings</button>
-        </Link>
-     </div>
-    
-  </div>
-    
+      <Link to="/EventActivityItineraryBooking">
+        <button style={{ padding: '10px', margin: '10px', fontSize: '16px' }}>Book a ticket to an activity</button>
+      </Link>
+    </div>
   );
-};
-
-const buttonStyle = {
-  margin: '0 10px',
-  padding: '10px 20px',
-  fontSize: '16px',
-  cursor: 'pointer',
-  borderRadius: '5px',
-  border: '1px solid #ccc',
 };
 
 export default UpcomingActivities;
