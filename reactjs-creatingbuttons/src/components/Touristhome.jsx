72fd00ea9a0db5c6137da1be583dc55e34baf4c0
basantabdalhamed <161749868+basantabdalhamed@users.noreply.github.com>
2024-12-01 20:50:16 +0200

    Basant Commit

--- reactjs-creatingbuttons/src/components/Touristhome.jsx
+++ reactjs-creatingbuttons/src/components/Touristhome.jsx
@@ -1,83 +1,100 @@
-// Touristhome.jsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import BirthdayPromoModal from './BirthdayPromoModal'; // Import the modal
+import './Touristhome.css'; // Import the CSS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faExclamation, faCircleXmark, faLock, faPlane, faBus, faCity, faBinoculars, faHotel, faCar, faGripLines } from '@fortawesome/free-solid-svg-icons'; // Import the grip lines icon
 
 export default function Touristhome() {
   const navigate = useNavigate();
-  const [showPromoModal, setShowPromoModal] = useState(true); // Initially show the modal
+  const [showPromoModal, setShowPromoModal] = useState(true);
 
-  const handleClosePromoModal = () => {
-    setShowPromoModal(false);
-  };
+  const handleClosePromoModal = () => setShowPromoModal(false);
 
   useEffect(() => {
-    // Simulating it always being the user's birthday to show the modal
-    setShowPromoModal(true); 
+    setShowPromoModal(true);
   }, []);
 
-  const handleUpdateProfileClick = () => {
-    navigate('/tourist-profile');
-  };
-
-  const handleViewComplaintsClick = () => {
-    navigate('/complaints');
-  };
-
-  const handleFileComplaintClick = () => {
-    navigate('/file-complaint');
-  };
-
-  const handleAccountDeletionClick = () => {
-    navigate('/request-account-deletion');
-  };
-
-  const handleBookTransportationClick = () => {
-    navigate('/book-transportation');
-  };
-
-  const handleBookFlightClick = () => {
-    navigate('/book-flight');
-  };
-
-  const handleViewPreviousActivitiesClick = () => {
-    navigate('/tourist-previous-activities');
-  };
-
-  const handleViewPastItinerariesClick = () => {
-    navigate('/tourist-past-itineraries');
-  };
-
-  const handleViewPastActivitiesClick = () => {
-    navigate('/tourist-previous-activities');
-  };
-
-  const handleBookHotelClick = () => {
-    navigate('/book-hotel');
-  };
-
-  const handleViewBookedTransportationsClick = () => {
-    navigate('/view-booked-transportations');
-  };
+  const handleUpdateProfileClick = () => navigate('/tourist-profile');
+  const handleViewComplaintsClick = () => navigate('/complaints');
+  const handleFileComplaintClick = () => navigate('/file-complaint');
+  const handleAccountDeletionClick = () => navigate('/request-account-deletion');
+  const handleBookTransportationClick = () => navigate('/book-transportation');
+  const handleBookFlightClick = () => navigate('/book-flight');
+  const handleViewPastItinerariesClick = () => navigate('/tourist-past-itineraries');
+  const handleViewPastActivitiesClick = () => navigate('/tourist-previous-activities');
+  const handleBookHotelClick = () => navigate('/book-hotel');
+  const handleViewBookedTransportationsClick = () => navigate('/view-booked-transportations');
+  
+  const handleHomeClick = () => navigate('/home');
+  const handleLogoutClick = () => navigate('/logout');
 
   return (
-    <header>
-      {/* Show the promo modal if showPromoModal is true */}
+    <div className="main-container">
+      {/* Background Image */}
+      <div className="background-image">
+        <header className="custom-header">
+          <div className="header-left">
+            <button onClick={handleHomeClick}>Home</button>
+            <button onClick={handleLogoutClick}>Log Out</button>
+            <button onClick={handleUpdateProfileClick}>View Profile</button>
+          </div>
+          <div className="header-right">
+            <h1>Explora</h1>
+          </div>
+        </header>
+      </div>
+
       {showPromoModal && <BirthdayPromoModal onClose={handleClosePromoModal} />}
+ {/* Services Text Section */}
+ <div className="services-section">
+        <FontAwesomeIcon icon={faGripLines} className="service-icon" /> {/* Updated icon */}
+        <span className="services-text">Services</span>
+        <FontAwesomeIcon icon={faGripLines} className="service-icon" /> {/* Updated icon */}
+      </div>
 
       <div className="button-container">
-        <button onClick={handleUpdateProfileClick}>Update my profile</button>
-        <button onClick={handleViewComplaintsClick}>View Issued Complaints</button>
-        <button onClick={handleFileComplaintClick}>File a Complaint</button>
-        <button onClick={handleAccountDeletionClick}>Request Account Deletion</button>
-        <button onClick={() => navigate('/change-password')}>Change My Password</button>
-        <button onClick={handleBookFlightClick}>Book Flight</button>
-        <button onClick={handleBookTransportationClick}>Book Transportation</button>
-        <button onClick={handleViewPastItinerariesClick}>View Past Itineraries</button>
-        <button onClick={handleViewPastActivitiesClick}>View Past Activities</button>
-        <button onClick={handleBookHotelClick}>Book Hotel</button>
-        <button onClick={handleViewBookedTransportationsClick}>View All My Booked Transportations</button>
+        <button onClick={handleUpdateProfileClick}>
+          <FontAwesomeIcon icon={faUser} />
+          Update Profile
+        </button>
+        <button onClick={handleViewComplaintsClick}>
+          <FontAwesomeIcon icon={faExclamation} />
+          View Issued Complaints
+        </button>
+        <button onClick={handleFileComplaintClick}>
+          <FontAwesomeIcon icon={faCircleXmark} />
+          File a Complaint
+        </button>
+        <button onClick={() => navigate('/change-password')}>
+          <FontAwesomeIcon icon={faLock} />
+          Change My Password
+        </button>
+        <button onClick={handleBookFlightClick}>
+          <FontAwesomeIcon icon={faPlane} />
+          Book Flight
+        </button>
+        <button onClick={handleBookTransportationClick}>
+          <FontAwesomeIcon icon={faBus} />
+          Book Transportation
+        </button>
+        <button onClick={handleViewPastItinerariesClick}>
+          <FontAwesomeIcon icon={faCity} />
+          View Past Itineraries
+        </button>
+        <button onClick={handleViewPastActivitiesClick}>
+          <FontAwesomeIcon icon={faBinoculars} />
+          View Past Activities
+        </button>
+        <button onClick={handleBookHotelClick}>
+          <FontAwesomeIcon icon={faHotel} />
+          Book Hotel
+        </button>
+        <button onClick={handleViewBookedTransportationsClick}>
+          <FontAwesomeIcon icon={faCar} />
+          View All My Booked Transportations
+        </button>
       </div>
-    </header>
+    </div>
   );
 }
