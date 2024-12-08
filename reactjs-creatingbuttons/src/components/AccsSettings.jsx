3357c3c243450716f868d2239e267b60a4f16a5b
basantabdalhamed <161749868+basantabdalhamed@users.noreply.github.com>
2024-12-08 23:40:03 +0200

    admin nav

--- reactjs-creatingbuttons/src/components/AccsSettings.jsx
+++ reactjs-creatingbuttons/src/components/AccsSettings.jsx
@@ -4,6 +4,16 @@ import './Touristhome.css'; // Import the CSS
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeadset, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import logo from '../assets/cropped_image.png';
+const buttonStyle = {
+  backgroundColor: '#008080',
+  color: 'white',
+  border: 'none',
+  padding: '10px 20px',
+  width: '100%',
+  textAlign: 'center',
+  cursor: 'pointer'
+};
 
 export default function Touristhome() {
   const navigate = useNavigate();
@@ -11,6 +21,23 @@ export default function Touristhome() {
   const [searchQuery, setSearchQuery] = useState(""); // Search query state
 
   const handleClosePromoModal = () => setShowPromoModal(false);
+  const [message, setMessage] = useState('');
+  const [loyaltyPoints, setLoyaltyPoints] = useState(0);
+  const [badgeLevel, setBadgeLevel] = useState('');
+  const [guideRatings, setGuideRatings] = useState({});
+  const [guideComments, setGuideComments] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [isEditable, setIsEditable] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isHistoryOptionsVisible, setIsHistoryOptionsVisible] = useState(false);
+
+  const toggleDropdown = () => setIsDropdownOpen(prev => !prev);
+  const handleMouseEnterHistory = () => setIsHistoryOptionsVisible(true);
+  const handleMouseLeaveHistory = () => setTimeout(() => setIsHistoryOptionsVisible(false), 900);
+
+  useEffect(() => {
+      setShowPromoModal(true);
+    }, []);
 
   useEffect(() => {
     setShowPromoModal(true);
@@ -95,6 +122,159 @@ export default function Touristhome() {
   const handleLogoutClick = () => navigate('/logout');
 
   return (
+    <div>
+           <nav
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: '1px 5px',
+          backgroundColor: '#008080',
+          color: 'white',
+          position: 'fixed',
+          top: 0,
+          width: '100%',
+          zIndex: 1000,
+          justifyContent: 'space-between',
+        }}
+      >
+        <img src={logo} alt="Logo" style={{ height: '80px', marginRight: '10px' }} />
+
+        {/* Navigation Links */}
+        <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+          <a href="/acc-settings" style={{ color: 'white', textDecoration: 'none' }}>Home</a>
+          <a href="/flagged-events" style={{ color: 'white', textDecoration: 'none' }}>Flag Event</a>
+          
+
+          <a href="/product-list" style={{ color: 'white', textDecoration: 'none' }}>Products</a>
+
+
+        </div>
+
+        {/* SVG Icon */}
+        <div style={{ marginLeft: 'auto', marginRight: '60px' }}>
+          <svg
+            onClick={toggleDropdown}
+            xmlns="http://www.w3.org/2000/svg"
+            width="54"
+            height="54"
+            viewBox="0 0 24 24"
+            style={{ cursor: 'pointer', color: 'white' }}
+          >
+            <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 22c-3.123 0-5.914-1.441-7.749-3.69.259-.588.783-.995 1.867-1.246 2.244-.518 4.459-.981 3.393-2.945-3.155-5.82-.899-9.119 2.489-9.119 3.322 0 5.634 3.177 2.489 9.119-1.035 1.952 1.1 2.416 3.393 2.945 1.082.25 1.61.655 1.871 1.241-1.836 2.253-4.628 3.695-7.753 3.695z" fill="white" />
+          </svg>
+          {/* Dropdown Menu */}
+          {isDropdownOpen && (
+            <div
+              style={{
+                position: 'absolute',
+                top: '80px',
+                right: '0',
+                backgroundColor: '#008080',
+                color: 'white',
+                borderRadius: '5px',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                width: '200px',
+                padding: '10px 0',
+                zIndex: 1000,
+              }}
+              onMouseEnter={handleMouseEnterHistory}
+              onMouseLeave={handleMouseLeaveHistory}
+            >
+              <button
+                onClick={() => navigate('/category-manager')}
+                style={buttonStyle}
+              >
+                Manage Activities
+              </button>
+              <button
+                onClick={() => navigate('/tag-manager')}
+                style={buttonStyle}
+              >
+                Manage Prefrences
+              </button>
+              <a
+                href="/history"
+                style={{
+                  color: 'white',
+                  textDecoration: 'none',
+                  textAlign: 'center',
+                  display: 'block',
+                }}
+              >
+                Manage Accounts
+              </a>
+
+              <button
+                onClick={() => navigate('/admin-documents')}
+                style={buttonStyle}
+              >
+                View Uploaded Documents
+              </button>
+              <button
+                onClick={() => navigate('/')}
+                style={buttonStyle}
+              >
+                Log Out
+              </button>
+              <button
+                onClick={() => navigate('/admin-complaints')}
+                style={buttonStyle}
+              >
+                View Complaints
+              </button>
+              <button
+                onClick={() => navigate('/viewDeleteRequests')}
+                style={buttonStyle}
+              >
+                View Delete Requests
+              </button>
+              <button
+                onClick={() => navigate('/promo-code')}
+                style={buttonStyle}
+              >
+                Create Promo Code
+              </button>
+            
+
+              {isHistoryOptionsVisible && (
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: '80px',
+                    right: '220px',
+                    backgroundColor: '#008080',
+                    color: 'white',
+                    borderRadius: '5px',
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                    width: '200px',
+                    padding: '10px 0',
+                    zIndex: 1000,
+                  }}
+                >
+                  <button
+                    onClick={() => navigate('/admin-view-users')}
+                    style={buttonStyle}
+                  >
+Delete Accounts                  </button>
+                  <button
+                    onClick={() => navigate('/login-tourism')}
+                    style={buttonStyle}
+                  >
+Add Tourism Governor                  </button>
+                  <button
+                    onClick={() => navigate('/login-admin')}
+                    style={buttonStyle}
+                  >
+Add Admin                  </button>
+                  
+
+                  
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </nav>
     <div className="main-container">
       {/* Background Image */}
       <div className="background-image">
@@ -233,6 +413,7 @@ export default function Touristhome() {
           <p>© Your Site Name, All Rights Reserved. Designed by HTML Codex</p>
         </div>
       </footer>
+    </div>
     </div>
   );
 }
